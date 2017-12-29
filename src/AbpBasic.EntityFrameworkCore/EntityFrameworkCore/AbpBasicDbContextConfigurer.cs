034da40ca8737a70165519c923ec792b5fd88cf4
tangdezhou <tangdezhou@yun-kai.com>
2017-12-29 18:24:09 +0800

    v0.2 ef迁移到mysql数据库

--- src/AbpBasic.EntityFrameworkCore/EntityFrameworkCore/AbpBasicDbContextConfigurer.cs
+++ src/AbpBasic.EntityFrameworkCore/EntityFrameworkCore/AbpBasicDbContextConfigurer.cs
@@ -7,12 +7,14 @@ namespace AbpBasic.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<AbpBasicDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            //builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<AbpBasicDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            //builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
